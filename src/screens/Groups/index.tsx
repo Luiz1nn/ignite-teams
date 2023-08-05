80c7c5cc55e983893044562048688a2726f5d9f2
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-05 01:02:58 -0300

    feat(Highlight): added component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
-import { Header } from '@/components'
+import { Header, Highlight } from '@/components'
 import { Container } from './styles'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
     </Container>
   )
 }
