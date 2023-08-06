a5a4166999dc58ec21077f9ef2bcc23667764afc
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-06 15:33:15 -0300

    feat(Groups): added FlatList Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,21 @@
 import { GroupCard, Header, Highlight } from '@/components'
 import { Container } from './styles'
+import { FlatList } from 'react-native'
+import { useState } from 'react'
 
 export function Groups() {
+  const [groups, setGroups] = useState<Array<string>>(['Galerinha do bem'])
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com sua turma" />
-      <GroupCard title="Galera do Ignite" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
