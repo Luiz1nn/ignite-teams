f7e05d54023296213c973a3574947528a6e47b4e
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-26 11:53:09 -0300

    chore: add stack navigation

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,14 @@
+import { Groups, NewGroup, Players } from '@/screens'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="groups" component={Groups} />
+      <Screen name="new" component={NewGroup} />
+      <Screen name="players" component={Players} />
+    </Navigator>
+  )
+}
